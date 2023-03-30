89459d1ee7e32b9ad56b33ce9ac70d3016020365
SEEWON <andyaaa78@gmail.com>
2023-03-30 07:12:30 -0700

    Phase 1작성을 위한 틀

--- phase1/myshell.c
+++ phase1/myshell.c
@@ -12,10 +12,9 @@ int main()
 {
     char cmdline[MAXLINE]; /* Command line */
 
-
     while (1) {
 	/* Read */
-	printf("> ");                   
+	printf("CSE4100-MP-PL> ");                   
 	fgets(cmdline, MAXLINE, stdin); 
 	if (feof(stdin))
 	    exit(0);
@@ -36,18 +35,22 @@ void eval(char *cmdline)
     pid_t pid;           /* Process id */
     
     strcpy(buf, cmdline);
-    bg = parseline(buf, argv); 
+    bg = parseline(buf, argv);  /* parseline() returns 1 if bg, or blank line input */
     if (argv[0] == NULL)  
-	return;   /* Ignore empty lines */
+	    return;   /* Ignore empty lines */
     if (!builtin_command(argv)) { //quit -> exit(0), & -> ignore, other -> run
-        if (execve(argv[0], argv, environ) < 0) {	//ex) /bin/ls ls -al &
-            printf("%s: Command not found.\n", argv[0]);
-            exit(0);
+        if((pid=Fork())==0) {
+            if (execve(argv[0], argv, environ) < 0) {	//ex) /bin/ls ls -al &
+                printf("%s: Command not found.\n", argv[0]);
+                exit(0);
+            }
+            // execvp(argv[0],argv);
         }
 
 	/* Parent waits for foreground job to terminate */
 	if (!bg){ 
 	    int status;
+        if (waitpid(pid, &status, 0) < 0) unix_error("waitfg: waitpid error");
 	}
 	else//when there is backgrount process!
 	    printf("%d %s", pid, cmdline);
@@ -59,10 +62,20 @@ void eval(char *cmdline)
 int builtin_command(char **argv) 
 {
     if (!strcmp(argv[0], "quit")) /* quit command */
-	exit(0);  
-    if (!strcmp(argv[0], "&"))    /* Ignore singleton & */
-	return 1;
-    return 0;                     /* Not a builtin command */
+	    exit(0);
+    else if (!strcmp(argv[0], "exit"))
+        exit(0);
+    else if (!strcmp(argv[0], "&"))    /* Ignore singleton & */
+	    return 1;
+    else if (!strcmp(argv[0], "cd")) {
+
+        return 1;
+    }
+    else if (!strcmp(argv[0], "history")) {
+
+        return 1;
+    }
+    else return 0;                     /* Not a builtin command */
 }
 /* $end eval */
 
@@ -74,17 +87,17 @@ int parseline(char *buf, char **argv)
     int argc;            /* Number of args */
     int bg;              /* Background job? */
 
-    buf[strlen(buf)-1] = ' ';  /* Replace trailing '\n' with space */
-    while (*buf && (*buf == ' ')) /* Ignore leading spaces */
-	buf++;
+    buf[strlen(buf)-1] = ' ';       /* Replace trailing '\n' with space */
+    while (*buf && (*buf == ' '))   /* Ignore leading spaces */
+	    buf++;                      /* By making buffer to point address of next character by adding 1*/
 
     /* Build the argv list */
     argc = 0;
     while ((delim = strchr(buf, ' '))) {
-	argv[argc++] = buf;
-	*delim = '\0';
-	buf = delim + 1;
-	while (*buf && (*buf == ' ')) /* Ignore spaces */
+	    argv[argc++] = buf;
+	    *delim = '\0';
+	    buf = delim + 1;
+	    while (*buf && (*buf == ' ')) /* Ignore spaces */
             buf++;
     }
     argv[argc] = NULL;
